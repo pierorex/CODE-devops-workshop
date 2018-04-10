d7a3e1138d2e8d9951784abd211ed94069ddb353
Piero Hernandez <jeanpierogm@gmail.com>
2018-04-10 11:29:13 +0200

    add test for method 'valid' and test for division

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -7,15 +7,23 @@ from calculator.logic import ValueTooLowException, ValueTooHighException
 class CalculatorTests(TestCase):
     def test_mul(self):
         calc = Calculator(-100000, 100000)
-        assert (calc.mul(1, 1) == 1)
-        assert (calc.mul(1, 0) == 0)
-        assert (calc.mul(0, 1) == 0)
-        assert (calc.mul(100, 10) == 1000)
-        assert (calc.mul(1, 1) == 1)
-        self.assertRaises(ValueTooLowException, lambda: calc.mul(-100000000000, 1))
-        self.assertRaises(ValueTooHighException, lambda: calc.mul(100000000000, 1))
-        self.assertRaises(ValueTooLowException, lambda: calc.mul(1, -100000000000))
-        self.assertRaises(ValueTooHighException, lambda: calc.mul(1, 100000000000))
+        self.assertEqual(calc.mul(1, 1), 1)
+        self.assertEqual(calc.mul(1, 0), 0)
+        self.assertEqual(calc.mul(0, 1), 0)
+        self.assertEqual(calc.mul(100, 10), 1000)
+        self.assertEqual(calc.mul(1, 1), 1)
+
+    def test_valid(self):
+        calc = Calculator(-100000, 100000)
+        self.assertRaises(ValueTooLowException, lambda: calc.div(-100000000000, 1))
+        self.assertRaises(ValueTooHighException, lambda: calc.div(100000000000, 1))
+        self.assertRaises(ValueTooLowException, lambda: calc.div(1, -100000000000))
+        self.assertRaises(ValueTooHighException, lambda: calc.div(1, 100000000000))
 
     def test_div(self):
-        pass
+        calc = Calculator(-100000, 100000)
+        self.assertEqual(calc.div(1, 1), 1)
+        self.assertEqual(calc.div(0, 1), 0)
+        self.assertEqual(calc.div(100, 10), 1000)
+        self.assertEqual(calc.div(1, 1), 1)
+        self.assertRaises(ZeroDivisionError, lambda: calc.div(1, 0))
