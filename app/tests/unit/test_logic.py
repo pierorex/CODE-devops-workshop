43f98edefe8f4789e67f2b61e06f4aefec111539
Piero Hernandez <jeanpierogm@gmail.com>
2018-04-10 11:21:55 +0200

    add tests for multiplication

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -1,10 +1,21 @@
 from unittest import TestCase
-from calculator.logic import Calculator
+from app.calculator.logic import Calculator
+
+from app.calculator.logic import ValueTooLowException, ValueTooHighException
 
 
 class CalculatorTests(TestCase):
     def test_mul(self):
-        pass
+        calc = Calculator(-100000, 100000)
+        assert (calc.mul(1, 1) == 1)
+        assert (calc.mul(1, 0) == 0)
+        assert (calc.mul(0, 1) == 0)
+        assert (calc.mul(100, 10) == 1000)
+        assert (calc.mul(1, 1) == 1)
+        self.assertRaises(ValueTooLowException, lambda: calc.mul(-100000000000, 1))
+        self.assertRaises(ValueTooHighException, lambda: calc.mul(100000000000, 1))
+        self.assertRaises(ValueTooLowException, lambda: calc.mul(1, -100000000000))
+        self.assertRaises(ValueTooHighException, lambda: calc.mul(1, 100000000000))
 
     def test_div(self):
         pass
